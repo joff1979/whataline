e82c453a591c7e1bd63c4e99884468557b670e46
Jamie Rollinson <joff1979@gmail.com>
2024-08-19 19:45:33 +0100

    testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,19 @@
 <script lang="ts">
     import About from "./layout/about.svelte";
-    import Awards from "./layout/awards.svelte";
-    import Header from "./layout/header.svelte";
-    import WorksContainer from "./layout/works-container.svelte";
-
 
 	export let name: string;
 </script>
 
 <header>
-	<svelte:component this={Header} />
 </header>
 
 
 
 
 <div class="flex items-center">
-	<WorksContainer/>
 	<About/>
 </div>
 
-<Awards/>
 
 
 <style global lang="postcss">
